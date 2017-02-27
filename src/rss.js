4b895e422a5e20e2a840664f9be1d79e205f95e9
kongou-ae <y.matsumoto.ae@gmail.com>
2017-02-27 20:11:13 +0000

    add rss

---
+++ src/rss.js
@@ -0,0 +1,37 @@
+'use strict';
+
+require('babel-polyfill');
+
+const RSS = require('rss');
+const requestPromise = require('request-promise');
+const fsPromise = require('fs-promise');
+
+const main = async() => {
+
+  const feed = new RSS({
+    title: "Anison.me",
+    feed_url: "https://i.anison.me/feed",
+    site_url: "https://i.anison.me"
+  });
+  
+  let options = {
+    uri: 'https://i.anison.me/anisonList.json',
+    json: true
+  };
+
+  let resp = await requestPromise(options)
+  
+  resp.forEach(song => {
+    let tmp = {}
+    tmp.title = song.collectionName + " / " + song.artistName
+    tmp.url = "https://i.anison.me/",
+    tmp.guid = song.collectionId,
+    tmp.date = song.releaseDate
+    feed.item(tmp)
+  })
+  
+  const xml = feed.xml({indent: true});
+  await fsPromise.writeFile('public/feed', xml)
+}
+
+main()
